fed8480ca7604ece7916e9f60593a214b1a260bd
mberodier <mberodier@gmail.com>
2022-05-16 12:03:03 +0200

    feat(questions): save question 1 to 10

--- index.js
+++ index.js
@@ -120,11 +120,38 @@ fetch('http://api.tvmaze.com/search/people?q=denzel+washington')
   .then(console.log);
 
 // Q16. Give me at least 3 memorable websites that engage to continue (because of nice UX/UI AND avoid to give me facebook, airbnb etc...)
+/*
+canva.com : Canva is a website that contains a great catalog of visuals (logos, presentation, posters..) which are editable and downloadable for free. The UI is very intuitive and provide people whithout design knowledge to make pro visuals.
 
+wetransfer.com : WeTransfer is a very well known websites for documents/photos sharing. It allows you to share documents with both computer and smartphone users by giving the receiver email. The UI is also very clean and enhance the user experience to me.
+
+linguee.fr : Linguee is a multi language translator which uses the DeepL algorithm. It's interface is very ergonomic and detailed regarding the definitions and examples provided. I'm using it since 5 years now and it is clearly a reference in term of translator. 
+
+*/
 // Q17. Describe an ESILV project that you worked on that you’re proud of?
-
+/*
+During this year, for my PI2 project, I had the opportunity to work with KnockKnock, a french cyber security company, and 3 random ESILV students. 
+The team synergy was fabulous and we managed to build a Python application that can test 4 different website vulnerability. 
+This project was very challenging and I feel very proud of the final results and feedbacks we obtained. 
+*/
 // Q18. Describe an ESILV project that you worked on that you’re not so proud of?
-
+/*
+I'm not proud of this year's WAA project. The project seemed very interresting regarding the diversity of computing skills involved, And I wish I managed to do better. 
+Besides I struggled a lot in focusing on this particular project as I had other ones to do at the same time. And because it was the most difficult for me (new coding language, new concept, new working method)
+However, I insist that the content of the course is very clear and motivating.
+*/
 // Q19. What are some things you like about the developer tools you use?
+/*
+My favourite coding language are SQL & Python.
 
+Python for its simplicity of use and for all the packages it contains. 
+Moreover, this language is used by a very large community always ready to help on the web, and this online mutual help is very pleasant to me. 
+
+SQL because I've got a particular interest in databases. I'm currently doing an internship in Allianz as a Data Platform Business Analyst, and I really love to browse big business databases.
+Besides, SQL isn't just a programming tool, but it also allows to analyse real performances by filtering virtual documented tables. 
+
+*/
 // Q20. Last question: could you explain me - in your terms - why the title of the course is "Web Application Architecture(s)" and not "Web Application Architecture"?
+/*
+
+*/
